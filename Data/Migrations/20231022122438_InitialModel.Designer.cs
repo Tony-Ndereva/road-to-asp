9f62c026906e6c3d29bde63cae6e38ba41b9476a
Tony Maluki <tonymaluki@gmail.com>
2023-10-22 23:02:19 +0300

    code-first migrations

--- Data/Migrations/20231022122438_InitialModel.Designer.cs
+++ Data/Migrations/20231022122438_InitialModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using road_to_asp.Data;
 
@@ -11,9 +12,11 @@ using road_to_asp.Data;
 namespace road_to_asp.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231022122438_InitialModel")]
+    partial class InitialModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
