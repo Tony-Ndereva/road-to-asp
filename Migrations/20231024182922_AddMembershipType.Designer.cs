fccdeecf709e7449ae3f7998ae3e22b201eebf42
Tony Maluki <tonymaluki@gmail.com>
2023-10-25 07:23:49 +0300

    membership types

--- Migrations/20231024182922_AddMembershipType.Designer.cs
+++ Migrations/20231024182922_AddMembershipType.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using road_to_asp.Data;
 
@@ -11,9 +12,11 @@ using road_to_asp.Data;
 namespace road_to_asp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231024182922_AddMembershipType")]
+    partial class AddMembershipType
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Models/MembershipType.cs
@@ -0,0 +1,10 @@
+﻿namespace road_to_asp.Models
+{
+    public class MembershipType
+    {
+        public byte MemberShipTypeId { get; set; }
+        public short SignUpFee { get; set; }
+        public byte DurationInMonths { get; set; }
+        public byte DiscountRate { get; set; }
+    }
+}
