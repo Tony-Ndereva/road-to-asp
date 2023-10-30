f3df4810739e1a1e1c899815a188d712e42f9c82
Tony Maluki <tonymaluki@gmail.com>
2023-10-30 23:15:39 +0300

    Working with forms

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -7,6 +7,7 @@ namespace road_to_asp.Models
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
+        [Display(Name = "Date of Birth")]
         public DateTime? BirthDate { get; set; }
         public bool IsSubscribedToNewsletter { get; set; }
         public MembershipType MembershipType { get; set; }
